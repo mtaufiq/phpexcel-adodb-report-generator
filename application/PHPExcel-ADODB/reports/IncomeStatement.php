2a75c3f79db1390a4250d31a0cb78c7d5d767bdb
drumbug20@yahoo.com <drumbug20@yahoo.com@4bf9a35a-1f0d-11df-8982-67e453f37615>
2010-02-28 00:53:11 +0000

    finished 2nd prototype. - accepts parameters format optionally. If none given, will create all formats - created main_template.php for global layout - added image icons for reports -

--- application/PHPExcel-ADODB/reports/IncomeStatement.php
+++ application/PHPExcel-ADODB/reports/IncomeStatement.php
@@ -1,11 +1,18 @@
 <?php //Creating the report
 $details = array();
 $details['fileName'] = "IncomeStatement";
-$details['format'] = "pdf";
+$details['format'] = $parameters['format'];
 $details['creator'] = "Penta Capital";
 $details['title'] = "Income Statement";
 
+/*
+REPORT GENERATOR - PLS MINIMIZE EDITING THIS CODE
+
+$objReport = a created object based on the 'rep' parameter
+$view = fetches the view data from the created report object
+
+*/
 $objReport = new $report($details , $parameters);
 $objReport->create();
-$view = $objReport->view() //get view report components (logs and html)
+$view = $objReport->view();
 ?>
