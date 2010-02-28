8d0a1c9dd5e2439d4acbee20eb0403c6469349e0
nayrusoul@yahoo.com <nayrusoul@yahoo.com@4bf9a35a-1f0d-11df-8982-67e453f37615>
2010-02-28 13:59:51 +0000

    edited ctr_IncomeStatement

--- trunk/application/PHPExcel-ADODB/controller/ctr_IncomeStatement.php
+++ trunk/application/PHPExcel-ADODB/controller/ctr_IncomeStatement.php
@@ -25,71 +25,47 @@ class IncomeStatement{
 		
 		$worksheet = $objPHPExcel->getActiveSheet();
 		
+		$DB = new IncomeStatementModel();
+		$incomeRows = $DB->get_Income($params['year'],$params['month']);
+		$expenseRows = $DB->get_Expense($params['year'],$params['month']);
+		
+		$worksheet->SetCellValue('B1', 'Penta Insurance Company');
 		$worksheet->SetCellValue('B2', 'Income Statement');
 		$monthName = date("F", mktime(0, 0, 0, $params['month'], 10)); 
 		$worksheet->SetCellValue('B3', 'For the end of '.$monthName);
-		$worksheet->SetCellValue('B5', 'Revenues');
-		$worksheet->SetCellValue('B6', 'Landscaping Fees');
-		$worksheet->SetCellValue('B7', 'Finance Charge Income');
-		$worksheet->SetCellValue('B9', 'Total');
-		$worksheet->SetCellValue('B11', 'Expenses');
-		$worksheet->SetCellValue('B12', 'Landscaping Fees');
-		$worksheet->SetCellValue('B13', 'Finance Charge Income');
-		$worksheet->SetCellValue('B15', 'Total');
-		$worksheet->SetCellValue('B18','Net Income');	
+		
+		$ctr = 5;
+		
+		while($row = $incomeRows->FetchRow()){
+		
+		$worksheet->SetCellValue('B'.$ctr, $row['Account']);
+		$worksheet->SetCellValue('C'.$ctr, $row['Debit']);
+		$worksheet->SetCellValue('D'.$ctr, $row['Credit']);
+		
+		$ctr++;
+		
+		}
+		
+		$ctr = $ctr + 1;
+		
+		while($row = $expenseRows->FetchRow()){
+		
+		$worksheet->SetCellValue('B'.$ctr, $row['Account']);
+		$worksheet->SetCellValue('C'.$ctr, $row['Debit']);
+		$worksheet->SetCellValue('D'.$ctr, $row['Credit']);
+		
+		$ctr++;
+		
+		}
+	
+		
 		$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
 		$this->report->end($this->report->getFormat());
-
-	}
-	
-	private function showSumFormula($totals){
 		
-		$formula = "=";
-		foreach($totals as $index):
-				$formula .= $index;
-				if(end($totals) <> $index){
-					$formula .= "+";
-				}
-		endforeach;
 		
-		return $formula;
-	
 	}
 
-	private function showAcctTotal($acct_type,$ctr,$totStart,$totEnd,$totalsC,$totalsD,$worksheet){
-						
-			$worksheet->getStyle('B'.$ctr.':'.'D'.$ctr)->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID);
-			$worksheet->getStyle('B'.$ctr.':'.'D'.$ctr)->getFill()->getStartColor()->setRGB('999000');
-			
-			$worksheet->getStyle('B'.$ctr)->getFont()->setBold(true);
-			$worksheet->SetCellValue('B'.$ctr, "Total ".$acct_type);
-			
-			$getSumOfC = 'SUM(C'.$totStart.':C'.$totEnd.')';
-			$getSumOfD = 'SUM(D'.$totStart.':D'.$totEnd.')';
-			$worksheet->SetCellValue('C'.$ctr, '='.$getSumOfC);	
-			$worksheet->SetCellValue('D'.$ctr, '='.$getSumOfD);
-			
-	}						
 	
-	private function checkAccountType($account){
-		if ($account == 'Assets' || $account == 'Income' || $account == 'Expense'){
-			return 'C'; //column for Debit
-		}else{
-			return 'D';
-		}
-	}
-	private function checkNegativeBalance($balance,$column,$account){
-		if(($account == 'Assets' || $account == 'Income') && $balance < 0){
-			return $this->reverseColumn($column);
-		}else{
-			return $column;
-		}
-	}
-	
-	private function reverseColumn($column){
-		if($column == 'C'){ return 'D'; }
-		else{ return 'C'; }
-	}
 	
 	public function view(){
 		$view['html'] = $this->report->view(); //load html components
