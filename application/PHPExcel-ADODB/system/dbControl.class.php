9fbb4fb8bf35ed5e55f894669514ebbe5d92beb2
drumbug20@yahoo.com <drumbug20@yahoo.com@4bf9a35a-1f0d-11df-8982-67e453f37615>
2010-03-04 12:03:30 +0000

    edited db control to use config data

--- application/PHPExcel-ADODB/system/dbControl.class.php
+++ application/PHPExcel-ADODB/system/dbControl.class.php
@@ -1,6 +1,5 @@
 <?php if ( !isset($GLOBALS['base_url'])) exit('No direct script access allowed');
 
-
 class dbControl{
 
 	var $host = "localhost";
@@ -11,7 +10,7 @@ class dbControl{
 	public function connect(){
 		$conn = &ADONewConnection('mysql'); 
 		$conn->SetFetchMode(ADODB_FETCH_ASSOC);
-		$conn->PConnect($this->host,$this->user,$this->pass,$this->db);
+		$conn->PConnect($GLOBALS['system']['host'],$GLOBALS['system']['user'],$GLOBALS['system']['pass'],$GLOBALS['system']['db']);
 		return $conn;
 	}
 	
