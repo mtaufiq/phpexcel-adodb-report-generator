5430c5178bad3da1b78c7227d2c7f728bac53608
drumbug20@yahoo.com <drumbug20@yahoo.com@4bf9a35a-1f0d-11df-8982-67e453f37615>
2010-04-13 16:10:11 +0000

    modified model for integration

--- trunk/application/PHPExcel-ADODB/model/mod_CashReceiptSummary.php
+++ trunk/application/PHPExcel-ADODB/model/mod_CashReceiptSummary.php
@@ -5,14 +5,14 @@ class CashReceiptSummaryModel{
 	public function get_summary($year,$month){
 		$db = new dbControl();
 		
-		$query = "Select account.account_type, account.account_desc , SUM(official_receipt_entries.debit) as debit , 
-						SUM(official_receipt_entries.credit) as credit
-				  From account, official_receipt_entries, official_receipt_reference	
-				  Where account.id = official_receipt_entries.account_id
-						AND official_receipt_entries.official_receipt_reference_id = official_receipt_reference.id
-						AND MONTH(official_receipt_reference.trans_date) = '".$month."'
-						AND YEAR(official_receipt_reference.trans_date) = '".$year."'
-				  GROUP BY account.account_desc";
+		$query = "Select acct_account.account_type, acct_account.account_desc , SUM(acct_official_receipt_entries.debit) as debit , 
+						SUM(acct_official_receipt_entries.credit) as credit
+				  From acct_account, acct_official_receipt_entries, acct_official_receipt_reference	
+				  Where acct_account.id = acct_official_receipt_entries.acct_account_id
+						AND acct_official_receipt_entries.acct_official_receipt_reference_id = acct_official_receipt_reference.id
+						AND MONTH(acct_official_receipt_reference.trans_date) = '".$month."'
+						AND YEAR(acct_official_receipt_reference.trans_date) = '".$year."'
+				  GROUP BY acct_account.account_desc";
 		
 		return $db->query($query);
 
