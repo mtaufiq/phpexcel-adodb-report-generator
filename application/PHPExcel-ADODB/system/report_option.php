c2054fbe3414392ff3e3ba3006edd0492913b278
jepoyoy@gmail.com <jepoyoy@gmail.com@4bf9a35a-1f0d-11df-8982-67e453f37615>
2010-06-11 08:33:01 +0000

    improved interface

---
+++ application/PHPExcel-ADODB/system/report_option.php
@@ -0,0 +1,18 @@
+<?php
+
+$redirect_string = "http://localhost/testExcel/PHPExcel-ADODB/view.php?";
+
+/*GET ALL PARAMETERS*/
+while ($rep_param = current($_POST)) {
+    
+    $rep_key = key($_POST);
+
+    	$redirect_string .= "".$rep_key."=".$rep_param."&";		
+
+    next($_POST);    
+}
+
+echo $redirect_string;
+
+header('Location: '.$redirect_string);
+?>
---
+++ application/PHPExcel-ADODB/web/js/collapseinit.js
@@ -0,0 +1,5 @@
+animatedcollapse.addDiv('yearly', 'fade=1', 'speed=1200');
+animatedcollapse.addDiv('monthly', 'fade=1', 'speed=1200');
+
+animatedcollapse.init();
+
